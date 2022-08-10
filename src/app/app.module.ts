31637d2e494e8b9f5dc1302724b07798824dde0f
Shelton Simbi <shltnsimbi@gmail.com>
2022-08-10 03:46:48 -0800

    added experience and skills section and its css

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon'
+import { MatExpansionModule } from '@angular/material/expansion'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -16,6 +18,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     MatGridListModule,
+    MatIconModule,
+    MatExpansionModule,
     BrowserAnimationsModule
   ],
   providers: [],
